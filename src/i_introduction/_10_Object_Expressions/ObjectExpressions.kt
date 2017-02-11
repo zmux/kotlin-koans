6b23c0a90c64df5193236f60cefda163b69ca7e3
Sergey Zmushko <zmuxsm@gmail.com>
2017-02-11 21:00:04 +0300

    i_introduction tasks are done

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -9,7 +9,8 @@ fun todoTask10(): Nothing = TODO(
         Task 10.
         Read about object expressions that play the same role in Kotlin as anonymous classes do in Java.
 
-        Add an object expression that provides a comparator to sort a list in a descending order using java.util.Collections class.
+        Add an object expression that provides a comparator to sort a list in a descending order using
+        java.util.Collections class.
         In Kotlin you use Kotlin library extensions instead of java.util.Collections,
         but this example is still a good demonstration of mixing Kotlin and Java code.
     """,
@@ -18,6 +19,10 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(o1: Int, o2: Int): Int {
+            return o2.compareTo(o1)
+        }
+    })
     return arrayList
 }
